ca67cd9360e5d9191548fc1824e9659214a3f730
Hong <hong_92@hotmail.my>
2020-10-20 14:02:31 +0800

    testing user update ( email and username pass)

--- instagram_web/blueprints/users/views.py
+++ instagram_web/blueprints/users/views.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, flash, redirect, url_for, request
 from models.user import User
-from flask_login import login_required, current_user
+from flask_login import login_required, current_user, login_user
 
 users_blueprint = Blueprint('users',
                             __name__,
@@ -27,7 +27,7 @@ def create():
         return redirect(url_for("users.new"))
 
 @users_blueprint.route('/<username>', methods=["GET"])
-#@login_required
+@login_required
 def show(username):
     if current_user.is_authenticated:
         print("hey")
@@ -41,13 +41,28 @@ def index():
 
 
 @users_blueprint.route('/<id>/edit', methods=['GET'])
+@login_required
 def edit(id):
-    pass
+    user = User.get_by_id(id)
+    if current_user ==  user:
+        return render_template('users/user_profile.html')
+    else:
+        return "Unauthorized to view others profile page"
 
 
 @users_blueprint.route('/<id>', methods=['POST'])
+@login_required
 def update(id):
-    pass
+
+    user = User.get_by_id(id)
+    data = request.form
+    update_user = User.update(username = data.get('username'),email = data.get('email')).where(User.id == id)
+    if update_user.execute():
+        flash("changed")
+        return redirect(url_for('users.edit', id=current_user.id))
+    else:
+        flash("something wrong")
+        return redirect(url_for('users.edit', id=current_user.id))
 
 
 
